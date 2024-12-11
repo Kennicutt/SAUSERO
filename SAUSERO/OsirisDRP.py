f7d984f50c78f987eda8066663b85bae616f8d5b
Kennicutt <fabricio.telescope@gmail.com>
2024-12-11 07:15:10 +0000

    Version prueba

--- SAUSERO/OsirisDRP.py
+++ SAUSERO/OsirisDRP.py
@@ -1,3 +1,18 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from reduction.reduction_osirisplus import *
 from aligning.aligning_osirisplus import *
 from astroMetry.astrometry_osirisplus import *
@@ -7,8 +22,8 @@ from astropy import units as u
 
 import argparse, time
 import os, json, warnings
+
 from Color_Codes import bcolors as bcl
-import logging
 from loguru import logger
 
 
