30147c9a8f8893b320ff509b9eddbd532e600d6f
Kennicutt <fabricio.telescope@gmail.com>
2024-12-19 07:33:20 +0000

    OK

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sausero",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(where='.'),
     package_data={
         'SAUSERO    ': [
